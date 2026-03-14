9502f4c539b8b55f7c2cb759710355127de7b71d
saurabhh1409 <saurabhnishad1409@gmail.com>
2026-03-14 18:44:13 +0530

    requirements addded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Saurabh",
+    author_email="saurabhnishad1409@gmail.com",
+    packages=find_packages()
+)
