f797d3035818ca0208d64ec82d18257688bbe0dd
saurabhh1409 <saurabhnishad1409@gmail.com>
2026-03-16 23:34:00 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,10 +1,6 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
 
-logging.info("This is a info message")
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-try:
-    a = 1/0
-except Exception as e:
-  raise USvisaException(e, sys)
+obj = TrainPipeline()
+obj.run_pipeline()
+
